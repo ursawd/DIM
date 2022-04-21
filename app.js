deda3a142e5e43e7f7a745d10ea88ed7dfa1f497
Phil Bailey <philbailey@msn.com>
2022-04-21 19:38:48 -0400

    add files page and nav buttons

--- app.js
+++ app.js
@@ -17,27 +17,52 @@ const storage = multer.diskStorage({
 		cb(null, file.originalname);
 	},
 });
+const storageFiles = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "./public/files");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
 
 const upload = multer({ storage: storage });
+const uploadFile = multer({ storage: storageFiles });
 
 const admin = false;
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
-// app.use(express.static("public"));
 
+// -------------------------------------------------
+// --process videos
 app.get("/", (req, res) => {
 	const filesFolder = "./public/videos";
 	const fileList = fs.readdirSync(filesFolder);
 	// fileList passed in via ejs object
-	res.render("upload", { fileList: fileList });
+	res.render("videos", { fileList: fileList });
 });
 
 app.post("/", upload.single("file"), (req, res) => {
 	// uploads file and then displays video files
 	res.redirect("/");
 });
+// -------------------------------------------------
+// --process files
+app.get("/files", (req, res) => {
+	const filesFolder = "./public/files";
+	const fileList = fs.readdirSync(filesFolder);
+	// fileList passed in via ejs object
+	res.render("files", { fileList: fileList });
+});
+
+app.post("/files", uploadFile.single("file"), (req, res) => {
+	// uploads file and then displays files
+	res.redirect("/files");
+});
+
+// -------------------------------------------------
 
 if (port) {
 	app.listen(port, "localhost", () => {
